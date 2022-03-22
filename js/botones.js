e9f5cde564757d566e5f43098ef198be62de69f8
Fernando Elias González Ortega <317014596@pcpuma.acatlan.unam.mx>
2022-03-22 14:11:22 -0600

    act

--- js/botones.js
+++ js/botones.js
@@ -1,23 +1,27 @@
-function mostrarImagen1(){
-    try {
-        var img = document.getElementById("B1");
-        var img2 = document.getElementById("B2");
-        img.style.visibility = "visible";
-        img2.style.visibility = "hidden";
-
-    } catch (error) {
-        alert(error);
-    }
+try {
+  const cent = 1;
+}
+catch (err) {
+  document.getElementById("demo").innerHTML = err;
 }
 
-function mostrarImagen2(){
-    try {
-        var img = document.getElementById("B1");
-        var img2 = document.getElementById("B2");
-        img.style.visibility = "hidden";
-        img2.style.visibility = "visible";
 
-    } catch (error) {
-        alert(error);
-    }
+
+
+function block() {
+  var z = document.createElement("img");
+
+  var x = document.getElementById("B1");
+  var y = document.getElementById("B2");
+ 
+    x.style.display = "none";
+    y.style.display = "block";
+  
+}
+function block2() {
+  var x = document.getElementById("B1");
+  var y = document.getElementById("B2");
+ 
+    y.style.display = "none";
+    x.style.display = "block";
 }
